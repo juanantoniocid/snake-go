8760899c763975da523be9ad2d4ed3bff4b55f9b
Juan Antonio Cid Tomas <juanantonio.cid@cabify.com>
2023-06-02 16:41:19 +0200

    game: Remove game logic from main

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"log"
-
-	"github.com/hajimehoshi/ebiten/v2"
-
 	"juanantoniocid/snake/internal/game"
 )
 
 func main() {
-	ebiten.SetWindowSize(game.ScreenWidth, game.ScreenHeight)
-	ebiten.SetWindowTitle("Snake [Zippo Games]")
-
-	if err := ebiten.RunGame(game.NewGame()); err != nil {
-		log.Fatal(err)
-	}
+	game.Run()
 }
